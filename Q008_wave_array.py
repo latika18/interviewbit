318b54fb27f0263ba1b594baf3207fc259a04e26
latika18 <34892423+latika18@users.noreply.github.com>
2018-05-16 09:03:39 +0530

    Create Q008_wave_array.py

---
+++ Q008_wave_array.py
@@ -0,0 +1,17 @@
+#Given an array of integers, sort the array into a wave like array and return it, 
+#In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
+
+#Example
+
+#Given [1, 2, 3, 4]
+
+#One possible answer : [2, 1, 4, 3]
+#Another possible answer : [4, 1, 3, 2]
+
+def wave(self, A):
+        
+        A.sort()
+        for i in range(0,len(A)-1,2):
+            A[i],A[i+1] = A[i+1],A[i]
+        return A
+            
