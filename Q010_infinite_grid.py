096d733df64c9598287659bb013b99c8e80736d8
latika18 <34892423+latika18@users.noreply.github.com>
2018-05-18 19:18:50 +0530

    Update Q010_infinite_grid.py

--- Q010_infinite_grid.py
+++ Q010_infinite_grid.py
@@ -18,3 +18,18 @@ Output : 2
 It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).
 
 This question is intentionally left slightly vague. Clarify the question by trying out a few cases in the “See Expected Output” section.
+CODE :
+ 
+ def cover_points(A, B):
+    
+    step = 0
+    for k in range(1,len(A)):
+        step_x =   abs(A[k]- A[k-1])
+        step_y =   abs(B[k]- B[k-1])
+        step += max(step_x,step_y)
+      
+    return step 
+
+   print cover_points([ 4, 8, -7, -5, -13, 9, -7, 8 ],[ 4, -15, -10, -3, -13, 12, 8, -8 ])
+print cover_points([ 2,3,4],[ 1,3,4 ])
+
