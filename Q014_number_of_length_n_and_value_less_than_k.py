16159bc699295cd0826896abd011110583e52656
latika18 <34892423+latika18@users.noreply.github.com>
2018-05-22 15:53:39 +0530

    Update Q014_number_of_length_n_and_value_less_than_k.py

--- Q014_number_of_length_n_and_value_less_than_k.py
+++ Q014_number_of_length_n_and_value_less_than_k.py
@@ -11,25 +11,41 @@ Constraints: 1 <= B <= 9, 0 <= C <= 1e9 & 0 <= A[i] <= 9
 .............................................................................................................
 Code :
 .............................................................................................................
-from itertools import combinations
+from itertools import product
+from itertools import ifilter
 def solve(A, B, C):
-    if len(A) < B:
+    if A == [] or B > len(str(C)):
         return 0
-    elif B > len(str(C)):
-        #return permutations less than C
-        return 0
-            
+
     elif B < len(str(C)):
-            #constraint is B
-        new_list = list(combinations(A,B))
-        print new_list
-        return len(new_list)
-                
-            
-            
+        #constraint is B
+        if B == 1:
+            new_list = A
+            return len(new_list)
+        else:
+            new_list = list((product((''.join(str(i)for i in A)),repeat = B)))
+            b = [''.join(num) for num in new_list]
+            c = list(ifilter(lambda  x: x[0]!='0'  , b))
+            return len(c)
+
+
     elif B == len(str(C)):
         #constraint is C 
-        pass
+        if B == 1:
+            new_list = [i  for i in A if i< C]
+            return len(new_list)
+        else:
+            new_list = list((product((''.join(str(i)for i in A)),repeat = B)))
+            b = [''.join(num) for num in new_list]
+            c = list(ifilter(lambda  x: x[0]!='0' and int(x) < C   , b))
+            return len(c)
+Test cases:
 
-print solve([0,1],1,5)
-            
+assert solve([2],5,51345) == 1
+assert solve([],1,1) == 0
+assert solve([ 2, 3, 5, 6, 7, 9 ],5,42950) == 2592
+assert solve([0],1,5) == 1
+assert solve([0,1,2,5],1,123) == 4
+assert solve([0,1,5],1,2) == 2
+assert solve([ 3 ],5, 26110) == 0
+assert solve([0,1,2,5],2,21) == 5
