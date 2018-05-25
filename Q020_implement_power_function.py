595063edb664dbb36974f0ea07e36bd1575a705e
latika18 <34892423+latika18@users.noreply.github.com>
2018-05-25 15:12:39 +0530

    Update Q020_implement_power_function.py

--- Q020_implement_power_function.py
+++ Q020_implement_power_function.py
@@ -11,3 +11,35 @@ Input : x = 2, n = 3, d = 3
 Output : 2
 
 2^3 % 3 = 8 % 3 = 2.
+
+def pow( x, n, d):
+    """returns remainder of x ^ n / d """
+    def power(x,n):
+        """returns x ^ n """        
+        x = abs(x)
+        if n == 0:
+            return 1
+        elif n == 1 :
+            return x
+        else:
+            for i in range(1,n):
+                x *= x
+                return x
+   
+    def mod(x,d):
+        """returns remainder of x /d""" 
+        x = abs(x)
+        if x == 1:
+            return d-1
+        else:
+            y = x/d
+            z = x - y*d
+            return z
+    
+    return mod(power(x,n),d)
+    
+assert pow( -1, 1, 20) == 19
+print pow( -1, 2, 20) == 
+
+
+print pow(71045970,41535484,64735492)
