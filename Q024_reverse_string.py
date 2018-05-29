d53b99639974e044f1686dd173c6cf4c97b52cda
latika18 <34892423+latika18@users.noreply.github.com>
2018-05-29 22:17:54 +0530

    Update Q024_reverse_string.py

--- Q024_reverse_string.py
+++ Q024_reverse_string.py
@@ -9,4 +9,18 @@ return "blue is sky the".
 A sequence of non-space characters constitutes a word.
 Your reversed string should not contain leading or trailing spaces, even if it is present in the input string.
 If there are multiple spaces between words, reduce them to a single space in the reversed string.
- NOTE: 
+Code:
+ 
+def reverseWords(A):
+    wordlist = []
+    x = len(A)
+    y = 0 ## len of previous string
+    for i in range(x):
+        if A[i] == ' ' :
+            wordlist.append(A[y:i])
+            y = i+1
+    wordlist.append(A[y:x])
+    return ' '.join(wordlist[::-1])
+
+#print reverseWords("The sky is blue")
+print reverseWords( "my name is latika agarwal")
