9c71408b39ab599696a803df8e7073eb572fb6d8
latika18 <34892423+latika18@users.noreply.github.com>
2018-06-02 21:34:04 +0530

    Update Q027_linkedlist_palindrome.py

--- Q027_linkedlist_palindrome.py
+++ Q027_linkedlist_palindrome.py
@@ -7,3 +7,30 @@ For example,
 
 List 1-->2-->1 is a palindrome.
 List 1-->2-->3 is not a palindrome.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param A : head node of linked list
+    # @return an integer
+    def lPalin(self, A):
+        stack = []
+        cnt = 1
+        curr = A   
+        prev = A
+        while A.next != None:
+            prev.next = A
+            cnt += 1
+              
+        for i in range(1, cnt//2 ):
+            if curr.val == A.val:
+                curr = curr.next 
+                A.next = prev
+                
+            else: 
+                return 0
+        return 1
