c63917ed7f13bd5a15ca956c93f6002fc3459d99
latika18 <34892423+latika18@users.noreply.github.com>
2018-06-02 21:42:47 +0530

    Update Q029_remove_duplicate_from_sorted_list.py

--- Q029_remove_duplicate_from_sorted_list.py
+++ Q029_remove_duplicate_from_sorted_list.py
@@ -4,3 +4,23 @@ Given a sorted linked list, delete all duplicates such that each element appear 
 For example,
 Given 1->1->2, return 1->2.
 Given 1->1->2->3->3, return 1->2->3.
+class Solution:
+    # @param A : head node of linked list
+    # @return an integer
+    def lPalin(self, A):
+        stack = []
+        cnt = 1
+        curr = A   
+        prev = A
+        while A.next != None:
+            prev.next = A
+            cnt += 1
+              
+        for i in range(1, cnt//2 ):
+            if curr.val == A.val:
+                curr = curr.next 
+                A.next = prev
+                
+            else: 
+                return 0
+        return 1
