ff1afd6ef78ee91320e6b0bd5765b63ea47cfddf
latika18 <34892423+latika18@users.noreply.github.com>
2018-06-06 20:39:18 +0530

    Update Q028_linkedlist_palindrome.py

--- Q028_linkedlist_palindrome.py
+++ Q028_linkedlist_palindrome.py
@@ -7,7 +7,7 @@ For example,
 
 List 1-->2-->1 is a palindrome.
 List 1-->2-->3 is not a palindrome.
-
+#final working solution on shell, also include code for reverse
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -17,29 +17,64 @@ List 1-->2-->3 is not a palindrome.
 class Solution:
     # @param A : head node of linked list
     # @return an integer
-    def lPalin(self, A):
-        ptr_P = A
-        ptr_Q = None
-        ispal = False
-        while ptr_P and ptr_P.next:
-            ptr_P = ptr_P.next.next 
-            ptr_Q = A
-            ptr_Q.next = ptr_Q
-            A = A.next
-        if ptr_P:
-            tail = A.next
+    def __init__(self,seq):
+         self.head = None
+         for item in seq:
+              node = ListNode(item)
+              node.next = self.head
+              self.head = node
+              
+    
+    def reverse(self):
+        current = self.head
+        prev = None
+        while (current is not None):
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+            
+        self.head = prev
+        reverse_list = []
+        while prev:
+            reverse_list.append(prev.val)
+            prev = prev.next
+        return reverse_list
+    
+    def lPalin(self):
+        node = self.head
+        fast = node
+        prev = None
+        ispal = True
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            temp = node.next
+            node.next = prev
+            prev = node
+            node = temp
+        
+        if fast:
+            tail = node.next
         else:
-            tail = A
-            
-        while ptr_Q:
-            isPal = False
-            if ptr_Q.val == tail.val:
+            tail = node
+      
+       
+        while prev and ispal:
+                      
+            if prev.val == tail.val:
                 tail = tail.next
-                ptr_Q = ptr_Q.next
+                prev = prev.next
                 ispal = True
-            
+            else:
+                 ispal = False
+                 break
+      
         if ispal :
             return 1
-            
         else :
             return 0
+
+listpal = Solution([7, 8, 6 ,  3 , 7 , 3 , 6, 8, 7 ])
+print listpal.reverse()
+print listpal.lPalin()
