938cb9f011e042800666d2a96017b828e0dbb3bb
latika18 <34892423+latika18@users.noreply.github.com>
2018-06-11 12:23:36 +0530

    Update Q037_reverse_linkedlist_using_recursion.py

--- Q037_reverse_linkedlist_using_recursion.py
+++ Q037_reverse_linkedlist_using_recursion.py
@@ -4,17 +4,18 @@
 ##Given 1->2->3->4->5->NULL,
 
 ##return 5->4->3->2->1->NULL.
-
-def Reverse_List_Recursion(self, p):
-        if p.next is None:
-            self.head = p
-            return
-        self.Reverse_List_Recursion(p.next)
-        q = p.next
-        q.next = p
-        p.next = None
-        
-    def reverseList(self, p):
-        self.head = p
-        self.Reverse_List_Recursion(p)
-        return self.head
+class Solution:
+    # @param A : string
+    # @return a list of strings
+def __init__(self):
+        self.table={1:["1"],0:["0"],2:["a","b","c"],3:["d","e","f"],4:["g","h","i"],5:["j","k","l"],6:["m","n","o"],
+                    7:["p","q","r","s"],8:["t","u","v"],9:["w","x","y","z"]    }
+    def letterCombinations(self, A):
+        ans=[]
+        if len(A)==1:
+            return self.table[int(A[0])]
+        for j in self.table[int(A[0])]:
+            temp=self.letterCombinations(A[1:])
+            for i in temp:
+                ans.append(j+i)
+        return ans
