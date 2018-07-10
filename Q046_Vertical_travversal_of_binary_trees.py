974eddf47b2449703bb4fa6beae05382ccb4298a
latika18 <34892423+latika18@users.noreply.github.com>
2018-07-10 21:34:59 +0530

    Create Q046_Vertical_travversal_of_binary_trees.py

---
+++ Q046_Vertical_travversal_of_binary_trees.py
@@ -0,0 +1,22 @@
+Given a binary tree, print a vertical order traversal of it.
+
+Example :
+Given binary tree:
+
+      6
+    /   \
+   3     7
+  / \     \
+ 2   5     9
+returns
+
+[
+    [2],
+    [3],
+    [6 5],
+    [7],
+    [9]
+]
+
+
+Note : If 2 Tree Nodes shares the same vertical level then the one with lesser depth will come first.
