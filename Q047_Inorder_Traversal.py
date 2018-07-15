01d42aa3f775fed4dd9bc67791871c5d32fff875
latika18 <34892423+latika18@users.noreply.github.com>
2018-07-15 21:56:40 +0530

    Update Q047_Inorder_Traversal.py

--- Q047_Inorder_Traversal.py
+++ Q047_Inorder_Traversal.py
@@ -9,3 +9,86 @@ Given binary tree
     /
    3
 return [1,3,2].
+##Question 26
+##Perform Breadth first traversal on a BST and print the elements traversed
+##Example Input:
+##100,12,98,223,-78,-12,82,96,74,30,789,912,120
+
+class Node(object):
+    def __init__(self, value,left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class BinarySearchTree(object):
+    def __init__(self, value):
+        self.root = Node(value)
+        
+
+    def insert(self, value):
+        current = self.root
+        while current:
+            if value > current.value:
+                if current.right is None:
+                    current.right = Node(value)
+                    break
+                else:
+                    current = current.right
+            else:
+                if current.left is None:
+                    current.left = Node(value)
+                    break
+                else:
+                    current = current.left
+
+            
+    def Breadth_first_search(self,root):
+        """In BFS the Node Values at each level of the Tree are traversed before going to next level"""
+
+        visited = []
+        if root:
+            visited.append(root)
+            print root.value
+        current = root
+        while current :
+            if current.left:
+                print current.left.value
+                visited.append(current.left)
+            if current.right:
+                print current.right.value
+                visited.append(current.right)
+            visited.pop(0)
+            
+            if not visited:
+                break
+            
+            
+            current = visited[0]
+            
+        
+               
+  
+t = BinarySearchTree(100)
+t.insert(12)
+t.insert(92)
+t.insert(112)
+t.insert(123)
+t.insert(2)
+t.insert(11)
+t.insert(52)
+t.insert(3)
+t.insert(66)
+t.insert(10)
+
+print "Output of Breadth First search is "
+t.Breadth_first_search(t.root)
+
+
+
+
+
+
+
+                    
+    
+
