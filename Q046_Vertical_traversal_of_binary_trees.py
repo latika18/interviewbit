21fdc6b6338e75827ed42c130a9e4d93b151e3c9
latika18 <34892423+latika18@users.noreply.github.com>
2018-08-04 23:33:15 +0530

    Update Q046_Vertical_traversal_of_binary_trees.py

--- Q046_Vertical_traversal_of_binary_trees.py
+++ Q046_Vertical_traversal_of_binary_trees.py
@@ -20,3 +20,41 @@ returns
 
 
 Note : If 2 Tree Nodes shares the same vertical level then the one with lesser depth will come first.
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param A : root node of tree
+    # @return a list of list of integers 
+    def __init__(self,root):
+        self.root = TreeNode(value)
+        
+    def insert(self,value):
+        current = A
+        while current:
+            if value > current.value:
+                if current.right is None:
+                    current.right = TreeNode(value)
+                    break
+                else:
+                    current = current.right
+            else:
+                if current.left is None:
+                    current.left = TreeNode(value)
+                    break
+                else:
+                    current = current.left
+                    
+        
+    
+    def verticalOrderTraversal(self, A):
+        if A.left:
+            self.verticalOrderTraversal(A.left)
+        
+        print root.value
+        if root.right:
+            self.verticalOrderTraversal(root.right)
