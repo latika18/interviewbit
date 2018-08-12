c2d0d50ebbd27d932597c39c16448aaf91d01b9c
latika18 <34892423+latika18@users.noreply.github.com>
2018-08-12 20:30:43 +0530

    Update Q050_postorder_traversal.py

--- Q050_postorder_traversal.py
+++ Q050_postorder_traversal.py
@@ -11,63 +11,7 @@ Given binary tree
    3
 return [3,2,1].
 
-Using recursion is not allowed.
 
-class Node(object):
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
-
-class BST(object):
-    def __init__(self, value):
-        self.root = Node(value)
-
-    def insert(self, value):
-        current = self.root
-        while current:
-            if value > current.value:
-                if current.right is None:
-                    current.right  = Node(value)
-                    break
-                else:
-                    current = current.right
-            else:
-                if current.left is None :
-                    current.left  = Node(value)
-                    break
-                else:
-                    current = current.left
-
-   
-     
-
-    def postorder(self,root):
-##      left ,right , root
-
-        if root:
-            if root.left:
-                self.postorder(root.left)
-            if root.right:
-                self.postorder(root.right)
-            print root.value
-
-
-t = BST(100)
-t.insert(12)
-t.insert(92)
-t.insert(112)
-t.insert(123)
-t.insert(2)
-t.insert(11)
-t.insert(52)
-t.insert(3)
-t.insert(66)
-t.insert(10)
-print t.root
-
-print "postorder traversal is this "
-t.postorder(t.root)
 # Definition for a  binary tree node
 # class TreeNode:
 #     def __init__(self, x):
